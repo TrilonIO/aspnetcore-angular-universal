b1dd9886d2cd2599a2c1c96fffa983e96f59d7f1
Jan Sviland <jan@sviland.org>
2017-03-30 14:15:09 +0200

    chat messages received in console

--- Client/app/containers/chat/chat.component.ts
+++ Client/app/containers/chat/chat.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit, Inject } from '@angular/core';
 
-import { SignalR } from 'ng2-signalr';
+import { SignalR, BroadcastEventListener, SignalRConnection } from 'ng2-signalr';
+import { Subscription } from "rxjs/Subscription";
+
+export class ChatMessage {
+    constructor(public user: string, public content: string) { }
+}
 
 @Component({
     selector: 'chat',
@@ -8,12 +13,26 @@ import { SignalR } from 'ng2-signalr';
 })
 export class ChatComponent implements OnInit {
 
+    public chatMessages: ChatMessage[] = [];
+
+    private _connection: SignalRConnection;
+    private _subscription: Subscription;
+
     constructor(private _signalR: SignalR) { }
 
     ngOnInit() {
 
+        let onMessageSent$ = new BroadcastEventListener<ChatMessage>('OnMessageSent');
+
         this._signalR.connect().then((c) => {
             console.log(c);
+            // register the listener
+            c.listen(onMessageSent$);
+        });
+
+        this._subscription = onMessageSent$.subscribe((chatMessage: ChatMessage) => {
+            this.chatMessages.push(chatMessage);
+            console.log('chat messages', this.chatMessages);
         });
 
     }
