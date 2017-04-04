cd4c168fdd198e8e86ddb59b61af39a58c922f46
Owen <owen-p@hotmail.com>
2017-04-04 01:40:52 +0100

    Add SQL Lite with contributers migration data

---
+++ Server/Data/DbInitializer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using AspCoreServer.Models;
+using AspCoreServer;
+
+namespace AspCoreServer.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(SpaDbContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.User.Any())
+            {
+                return;   // DB has been seeded
+            }
+            var users = new Users[]
+            {
+               new Users(){Name = "Mark Pieszak"},
+               new Users(){Name = "Abrar Jahin"},
+               new Users(){Name = "hakonamatata"},
+               new Users(){Name = "LiverpoolOwen"},
+               new Users(){Name = "Ketrex"},
+               new Users(){Name = "markwhitfeld"},
+               new Users(){Name = "daveo1001"},
+               new Users(){Name = "paonath"},
+               new Users(){Name = "nalex095"},
+               new Users(){Name = "ORuban"},
+               new Users(){Name = "Gaulomatic"}
+            };
+
+            foreach (Users s in users)
+            {
+                context.User.Add(s);
+            }
+            context.SaveChanges();
+        }
+    }
+}
