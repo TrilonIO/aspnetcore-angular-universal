904ad5ee37b881579195c7073c2db3370d76704d
Jan Sviland <jan@sviland.org>
2017-04-05 15:04:15 +0200

    I18n support - Can switch between English and Norwegian (#155)
    * added ng2-translate
    
    * added translate module to app.module
    
    * translation works but json location hardcoded...
    
    * added buttons for switching language
    
    * some more translations
    
    * switching between english and norwegian works
    
    Closes #111

--- Client/app/app.module.ts
+++ Client/app/app.module.ts
@@ -1,10 +1,14 @@
 ﻿import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { HttpModule } from '@angular/http';
+import { CommonModule, APP_BASE_HREF } from '@angular/common';
+import { HttpModule, Http } from '@angular/http';
 import { FormsModule } from '@angular/forms';
 
 import { Ng2BootstrapModule } from 'ng2-bootstrap';
+
+// i18n support
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
@@ -16,6 +20,13 @@ import { Ng2BootstrapComponent } from './containers/ng2-bootstrap-demo/ng2bootst
 
 import { LinkService } from './shared/link.service';
 import { ConnectionResolver } from './shared/route.resolver';
+
+export function createTranslateLoader(http: Http) {
+
+    // BUG: URLs requested via Http on the server must be absolute
+    // TODO: Should not hardcode localhost://5000, should use APP_BASE_HREF!!
+    return new TranslateHttpLoader(http, 'http://localhost:5000/assets/i18n/', '.json');
+}
 
 @NgModule({
     declarations: [
@@ -32,6 +43,15 @@ import { ConnectionResolver } from './shared/route.resolver';
         HttpModule,
         FormsModule,
         Ng2BootstrapModule.forRoot(), // You could also split this up if you don't want the Entire Module imported
+
+        // i18n support
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: (createTranslateLoader),
+                deps: [Http]
+            }
+        }),
 
         // App Routing
         RouterModule.forRoot([
@@ -80,7 +100,7 @@ import { ConnectionResolver } from './shared/route.resolver';
                 }
             },
             {
-                path: 'chat', component: ChatComponent, 
+                path: 'chat', component: ChatComponent,
                 // Wait until the resolve is finished before loading the Route
                 resolve: { connection: ConnectionResolver },
                 data: {
@@ -109,8 +129,9 @@ import { ConnectionResolver } from './shared/route.resolver';
         ])
     ],
     providers: [
-        LinkService, 
-        ConnectionResolver
+        LinkService,
+        ConnectionResolver,
+        TranslateModule
     ]
 })
 export class AppModule {
