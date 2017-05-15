62fa6fa867e9dcefa34a5c2fe86eee15f88eeb26
Jan Sviland <jan@sviland.org>
2017-05-15 10:29:22 +0200

    added decrease action

--- Client/app/containers/counter/counter.component.ts
+++ Client/app/containers/counter/counter.component.ts
@@ -1,24 +1,33 @@
 // tslint:disable:no-null-keyword
+// tslint:disable:semicolon
 
 import { Component } from '@angular/core';
-import { Store } from "@ngrx/store";
+import { Store } from '@ngrx/store';
 import * as fromRoot from '../../reducers';
 
-import { COUNTER_INCREASE } from "../../reducers/counter.reducer";
+import { COUNTER_INCREASE, COUNTER_DECREASE } from '../../reducers/counter.reducer';
+import { CounterState } from '../../reducers/counter.reducer';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
     selector: 'counter',
     templateUrl: './counter.component.html'
 })
 export class CounterComponent {
-    public currentCount = 0;
+    // public currentCount = 0;
+    public counter: Observable<CounterState>;
 
     constructor(private _store: Store<fromRoot.State>) {
-
+        this.counter = _store.select<CounterState>('counter')
     }
 
     public incrementCounter() {
         // this.currentCount++;
-        this._store.dispatch({ type: COUNTER_INCREASE, payload: null });
+        this._store.dispatch({ type: COUNTER_INCREASE, payload: null })
+    }
+
+    public decreaseCounter() {
+        // this.currentCount++;
+        this._store.dispatch({ type: COUNTER_DECREASE, payload: null })
     }
 }
