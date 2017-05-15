4c78af0fde380774a0e422ca577b85d0af008085
Jan Sviland <jan@sviland.org>
2017-05-15 10:30:22 +0200

    removed some comments

--- Client/app/reducers/index.ts
+++ Client/app/reducers/index.ts
@@ -2,38 +2,15 @@ import { compose } from "@ngrx/core";
 import { combineReducers, ActionReducer } from "@ngrx/store";
 import { storeFreeze } from 'ngrx-store-freeze';
 
-
 import * as fromRouter from '@ngrx/router-store';
 import * as fromCounter from '../reducers/counter.reducer';
 
-
-// reducers
-// import { notifications } from "app/_reducers/notification.reducer";
-// import { intro } from "app/_reducers/intro.reducer";
-// import { chat } from "app/_reducers/chat.reducer";
-
 export interface State {
-  // search: fromSearch.State;
-  // books: fromBooks.State;
-  // collection: fromCollection.State;
-  // layout: fromLayout.State;
   router: fromRouter.RouterState;
   counter: fromCounter.CounterState;
-  // notifications: NotificationList;
-  // statistics: CompanyStatistics;
 }
 
-
-// export const reducers = {
-//   user, notifications, intro, statistics, event, chat
-// };
-
-
 const reducers = {
-  // search: fromSearch.reducer,
-  // books: fromBooks.reducer,
-  // collection: fromCollection.reducer,
-  // layout: fromLayout.reducer,
   router: fromRouter.routerReducer,
   counter: fromCounter.reducer
 };
