41ad1bc65a49d968a9eef5a02e0a07e3403fd902
David Gnanasekaran <c.davidgnanasekaran@gmail.com>
2017-09-16 14:19:03 +0530

    packages updated to fix 1) Zone already loaded error, 2) issue with not-found component template load issue

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,7 @@ const webpack = require('webpack');
 const merge = require('webpack-merge');
 const AotPlugin = require('@ngtools/webpack').AotPlugin;
 const CheckerPlugin = require('awesome-typescript-loader').CheckerPlugin;
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 const { sharedModuleRules } = require('./webpack.additions');
 
@@ -56,6 +57,7 @@ module.exports = (env) => {
                 moduleFilenameTemplate: path.relative(clientBundleOutputDir, '[resourcePath]') // Point sourcemap entries to the original file locations on disk
             })
         ] : [
+            // new BundleAnalyzerPlugin(),
             // Plugins that apply in production builds only
             new webpack.optimize.UglifyJsPlugin(),
             new AotPlugin({
