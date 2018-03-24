3ab0d687e9741be891291a2246ea6ad88f41ce16
Rune Antonsen <rune@norbits.no>
2018-03-24 14:20:09 +0100

    fix(polyfills): web-animations-js should be in browser.polyfills.ts

--- ClientApp/polyfills/browser.polyfills.ts
+++ ClientApp/polyfills/browser.polyfills.ts
@@ -2,3 +2,4 @@ import './polyfills.ts';
 
 import 'zone.js/dist/zone';
 import 'reflect-metadata';
+// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
--- ClientApp/polyfills/polyfills.ts
+++ ClientApp/polyfills/polyfills.ts
@@ -21,8 +21,6 @@
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
 
-// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
-
 /** Evergreen browsers require these. **/
 import 'core-js/es6/reflect';
 import 'core-js/es7/reflect';
