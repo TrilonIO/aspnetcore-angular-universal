cc377108faacc8ed72a2a48ec9b462f45e4ff3c8
GRIMMR3AP3R <robert.grimm.213@outlook.com>
2018-06-07 11:42:45 -0500

    feat(PWA): add progressive web app manifest + PWA to app
    * PWA !!
    * Fixed the published PWA with workaround

--- Server/Controllers/HomeController.cs
+++ Server/Controllers/HomeController.cs
@@ -1,25 +1,26 @@
-using Asp2017.Server.Helpers;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-
-using Microsoft.AspNetCore.SpaServices.Prerendering;
-using Microsoft.AspNetCore.NodeServices;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http.Features;
-using Microsoft.AspNetCore.Http;
-using System.Diagnostics;
 using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Asp2017.Server.Helpers;
 using Asp2017.Server.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.NodeServices;
+using Microsoft.AspNetCore.SpaServices.Prerendering;
+using Microsoft.Extensions.DependencyInjection;
 
-namespace AspCoreServer.Controllers
-{
-  public class HomeController : Controller
-  {
+namespace AspCoreServer.Controllers {
+  public class HomeController : Controller {
+    protected readonly IHostingEnvironment HostingEnvironment;
+    public HomeController (IHostingEnvironment hostingEnv) {
+      this.HostingEnvironment = hostingEnv;
+    }
+
     [HttpGet]
-    public async Task<IActionResult> Index()
-    {
-      var prerenderResult = await Request.BuildPrerender();
+    public async Task<IActionResult> Index () {
+      var prerenderResult = await Request.BuildPrerender ();
 
       ViewData["SpaHtml"] = prerenderResult.Html; // our <app-root /> from Angular
       ViewData["Title"] = prerenderResult.Globals["title"]; // set our <title> from Angular
@@ -28,34 +29,35 @@ namespace AspCoreServer.Controllers
       ViewData["Meta"] = prerenderResult.Globals["meta"]; // set our <meta> SEO tags
       ViewData["Links"] = prerenderResult.Globals["links"]; // set our <link rel="canonical"> etc SEO tags
       ViewData["TransferData"] = prerenderResult.Globals["transferData"]; // our transfer data set to window.TRANSFER_CACHE = {};
+      if (!HostingEnvironment.IsDevelopment ()) {
+        ViewData["ServiceWorker"] = "<script>'serviceWorker'in navigator&&navigator.serviceWorker.register('/serviceworker')</script>";
+      }
 
-      return View();
+      return View ();
     }
 
     [HttpGet]
-    [Route("sitemap.xml")]
-    public async Task<IActionResult> SitemapXml()
-    {
+    [Route ("sitemap.xml")]
+    public IActionResult SitemapXml () {
       String xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?>";
 
       xml += "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">";
       xml += "<url>";
       xml += "<loc>http://localhost:4251/home</loc>";
-      xml += "<lastmod>" + DateTime.Now.ToString("yyyy-MM-dd") + "</lastmod>";
+      xml += "<lastmod>" + DateTime.Now.ToString ("yyyy-MM-dd") + "</lastmod>";
       xml += "</url>";
       xml += "<url>";
       xml += "<loc>http://localhost:4251/counter</loc>";
-      xml += "<lastmod>" + DateTime.Now.ToString("yyyy-MM-dd") + "</lastmod>";
+      xml += "<lastmod>" + DateTime.Now.ToString ("yyyy-MM-dd") + "</lastmod>";
       xml += "</url>";
       xml += "</urlset>";
 
-      return Content(xml, "text/xml");
+      return Content (xml, "text/xml");
 
     }
 
-    public IActionResult Error()
-    {
-      return View();
+    public IActionResult Error () {
+      return View ();
     }
   }
 }
--- Startup.cs
+++ Startup.cs
@@ -1,144 +1,124 @@
 using System;
 using System.IO;
+using AspCoreServer.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.SpaServices.Webpack;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.EntityFrameworkCore;
-using AspCoreServer.Data;
-using Swashbuckle.AspNetCore.Swagger;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Net.Http.Headers;
+using Swashbuckle.AspNetCore.Swagger;
+using WebEssentials.AspNetCore.Pwa;
 
-namespace AspCoreServer
-{
-  public class Startup
-  {
+namespace AspCoreServer {
+  public class Startup {
 
-    public static void Main(string[] args)
-    {
-      var host = new WebHostBuilder()
-          .UseKestrel()
-          .UseContentRoot(Directory.GetCurrentDirectory())
-          .UseIISIntegration()
-          .UseStartup<Startup>()
-          .Build();
+    public static void Main (string[] args) {
+      var host = new WebHostBuilder ()
+        .UseKestrel ()
+        .UseContentRoot (Directory.GetCurrentDirectory ())
+        .UseIISIntegration ()
+        .UseStartup<Startup> ()
+        .Build ();
 
-      host.Run();
+      host.Run ();
     }
-    public Startup(IHostingEnvironment env)
-    {
-      var builder = new ConfigurationBuilder()
-          .SetBasePath(env.ContentRootPath)
-          .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-          .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-          .AddEnvironmentVariables();
-      Configuration = builder.Build();
+    public Startup (IHostingEnvironment env) {
+      var builder = new ConfigurationBuilder ()
+        .SetBasePath (env.ContentRootPath)
+        .AddJsonFile ("appsettings.json", optional : true, reloadOnChange : true)
+        .AddJsonFile ($"appsettings.{env.EnvironmentName}.json", optional : true)
+        .AddEnvironmentVariables ();
+      Configuration = builder.Build ();
     }
 
     public IConfigurationRoot Configuration { get; }
 
     // This method gets called by the runtime. Use this method to add services to the container.
-    public void ConfigureServices(IServiceCollection services)
-    {
+    public void ConfigureServices (IServiceCollection services) {
       // Add framework services.
-      services.AddMvc();
-      services.AddNodeServices();
+      services.AddMvc ();
+      services.AddNodeServices ();
+      services.AddHttpContextAccessor ();
+      services.AddProgressiveWebApp (new PwaOptions { Strategy = ServiceWorkerStrategy.CacheFirst, RegisterServiceWorker = true, RegisterWebmanifest = true }, "manifest.json");
 
       var connectionStringBuilder = new Microsoft.Data.Sqlite.SqliteConnectionStringBuilder { DataSource = "spa.db" };
-      var connectionString = connectionStringBuilder.ToString();
+      var connectionString = connectionStringBuilder.ToString ();
 
-      services.AddDbContext<SpaDbContext>(options =>
-          options.UseSqlite(connectionString));
+      services.AddDbContext<SpaDbContext> (options =>
+        options.UseSqlite (connectionString));
 
       // Register the Swagger generator, defining one or more Swagger documents
-      services.AddSwaggerGen(c =>
-      {
-        c.SwaggerDoc("v1", new Info { Title = "Angular 6.0 Universal & ASP.NET Core advanced starter-kit web API", Version = "v1" });
+      services.AddSwaggerGen (c => {
+        c.SwaggerDoc ("v1", new Info { Title = "Angular 6.0 Universal & ASP.NET Core advanced starter-kit web API", Version = "v1" });
       });
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, SpaDbContext context)
-    {
-      loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-      loggerFactory.AddDebug();
+    public void Configure (IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, SpaDbContext context) {
+      loggerFactory.AddConsole (Configuration.GetSection ("Logging"));
+      loggerFactory.AddDebug ();
 
       // app.UseStaticFiles();
 
-      app.UseStaticFiles(new StaticFileOptions()
-      {
-        OnPrepareResponse = c =>
-        {
+      app.UseStaticFiles (new StaticFileOptions () {
+        OnPrepareResponse = c => {
           //Do not add cache to json files. We need to have new versions when we add new translations.
 
-          if (!c.Context.Request.Path.Value.Contains(".json"))
-          {
-            c.Context.Response.GetTypedHeaders().CacheControl = new CacheControlHeaderValue()
-            {
-              MaxAge = TimeSpan.FromDays(30) // Cache everything except json for 30 days
+          if (!c.Context.Request.Path.Value.Contains (".json")) {
+          c.Context.Response.GetTypedHeaders ().CacheControl = new CacheControlHeaderValue () {
+          MaxAge = TimeSpan.FromDays (30) // Cache everything except json for 30 days
             };
-          }
-          else
-          {
-            c.Context.Response.GetTypedHeaders().CacheControl = new CacheControlHeaderValue()
-            {
-              MaxAge = TimeSpan.FromMinutes(15) // Cache json for 15 minutes
+          } else {
+            c.Context.Response.GetTypedHeaders ().CacheControl = new CacheControlHeaderValue () {
+              MaxAge = TimeSpan.FromMinutes (15) // Cache json for 15 minutes
             };
           }
         }
       });
 
-      DbInitializer.Initialize(context);
+      DbInitializer.Initialize (context);
 
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-        app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
-        {
+      if (env.IsDevelopment ()) {
+        app.UseDeveloperExceptionPage ();
+        app.UseWebpackDevMiddleware (new WebpackDevMiddlewareOptions {
           HotModuleReplacement = true,
-          HotModuleReplacementEndpoint = "/dist/"
+            HotModuleReplacementEndpoint = "/dist/"
         });
-        app.UseSwagger();
-        app.UseSwaggerUI(c =>
-        {
-          c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+        app.UseSwagger ();
+        app.UseSwaggerUI (c => {
+          c.SwaggerEndpoint ("/swagger/v1/swagger.json", "My API V1");
         });
 
         // Enable middleware to serve swagger-ui (HTML, JS, CSS etc.), specifying the Swagger JSON endpoint.
 
-
-        app.MapWhen(x => !x.Request.Path.Value.StartsWith("/swagger", StringComparison.OrdinalIgnoreCase), builder =>
-        {
-          builder.UseMvc(routes =>
-          {
-            routes.MapSpaFallbackRoute(
-                name: "spa-fallback",
-                defaults: new { controller = "Home", action = "Index" });
+        app.MapWhen (x => !x.Request.Path.Value.StartsWith ("/swagger", StringComparison.OrdinalIgnoreCase), builder => {
+          builder.UseMvc (routes => {
+            routes.MapSpaFallbackRoute (
+              name: "spa-fallback",
+              defaults : new { controller = "Home", action = "Index" });
           });
         });
-      }
-      else
-      {
-        app.UseMvc(routes =>
-        {
-          routes.MapRoute(
-           name: "default",
-           template: "{controller=Home}/{action=Index}/{id?}");
+      } else {
+        app.UseMvc (routes => {
+          routes.MapRoute (
+            name: "default",
+            template: "{controller=Home}/{action=Index}/{id?}");
 
-          routes.MapRoute(
-           "Sitemap",
-           "sitemap.xml",
-           new { controller = "Home", action = "SitemapXml" });
+          routes.MapRoute (
+            "Sitemap",
+            "sitemap.xml",
+            new { controller = "Home", action = "SitemapXml" });
 
-          routes.MapSpaFallbackRoute(
+          routes.MapSpaFallbackRoute (
             name: "spa-fallback",
-            defaults: new { controller = "Home", action = "Index" });
+            defaults : new { controller = "Home", action = "Index" });
 
         });
-        app.UseExceptionHandler("/Home/Error");
+        app.UseExceptionHandler ("/Home/Error");
       }
     }
   }
