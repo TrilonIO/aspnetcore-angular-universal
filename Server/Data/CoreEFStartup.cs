837b134516631afb3034d40278e2bc9f170acdba
Detys <k.santrou@gmail.com>
2019-05-27 02:49:07 +0300

    fix(seeding): seeding now follows .NET Core 2.0 best practices

---
+++ Server/Data/CoreEFStartup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AspCoreServer.Data
+{
+    public static class CoreEFStartup
+    {
+        public static async Task InitializeDatabaseAsync(IServiceProvider services)
+        {            
+            var context = services.GetRequiredService<SpaDbContext>();
+
+            await context.Database.EnsureCreatedAsync();
+        }
+
+    }
+}
